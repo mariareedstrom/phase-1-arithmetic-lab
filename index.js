4c5e0016ac131823c3f647cc0a6e80627810b420
Maria Reedstrom <maria.reedstrom@gmail.com>
2021-10-13 14:17:34 -0500

    commit tree f4cc872ee5b501608f2906c45fd3b2d46d7f65bc

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 
-// Write your code here
+var num1 = 31;
+var num2 = 2;
+
+const multiply = num1 * num2;
+
+multiply;
+
+var random = Math.floor(Math.random()*10);
+
+random;
+
+
+let num3 = 19;
+let num4 = 5; 
+
+var mod = Math.ceil(num3 / num4);  
+
+mod; 
+
+
+var max = Math.max(5, 10, 15, 20);
+
+max;
